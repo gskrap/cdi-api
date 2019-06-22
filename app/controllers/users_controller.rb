ff330d32152f447399a8f40976fe0fa43bbd210d
Melissa Strain <melissastrain@MelissasMacBook.lan1>
2019-06-22 17:24:16 -0400

    add archived to permitted user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -92,7 +92,7 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :date_of_birth, :email, :bio, :phone, :role, :gender, :alumni, :password)
+      params.require(:user).permit(:first_name, :last_name, :date_of_birth, :email, :bio, :phone, :role, :gender, :alumni, :password, :archived)
     end
 
     def emergency_contact_params
