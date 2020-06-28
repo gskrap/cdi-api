41171b19fccff915312aeb901fa6a3dedbe1d1be
George Skrapits <gskrap@gmail.com>
2020-06-27 20:37:10 -0400

    fix: user update endpoint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   def update
     if @user.update(user_params[:user])
+      @user.save
       render json: @user, :except=>  [:auth_token, :password_digest]
     else
       render json: @user.errors, status: :unprocessable_entity
